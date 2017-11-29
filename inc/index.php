fe75287415d2e2a42553845a5cb4e4a355982552
PikaJew002 <aaron@ironmthome.com>
2017-11-29 00:30:43 -0500

    Update header content text

--- inc/index.php
+++ inc/index.php
@@ -3,9 +3,12 @@
     <div id="content">
       <h1>The one-stop-shop for all of your conference needs</h1>
       <p>
-        The Conference Manager provides a complete suite of solutions for your conference needs.
-        Paper reviews can register personalized accounts with a live bidding system for choosing
-        which papers they choose to review with restraints specified by you, the administrator.
+        The Conference Manager provides a complete suite of solutions for your conference needs:
+        <ul>
+          <li>Paper Submission Management</li>
+          <li>Paper Review Management</li>
+          <li>Conference Registration and Check-In</li>
+        </ul>
       </p>
     </div>
   </div>
