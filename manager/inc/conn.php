f3ff254fcf196dd74341f23527e83c7150b1f4c6
PikaJew002 <aaron@ironmthome.com>
2017-11-29 18:06:19 -0500

    Added db connection file to /manager/ pages.

---
+++ manager/inc/conn.php
@@ -0,0 +1,6 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "admin", "conference_manager");
+if($mysqli->connect_error) {
+  header("Location: ../index.php?page=login&error_msg=db_conn_fail");
+}
+?>
--- manager/index.php
+++ manager/index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require("inc/auth.php");
+require("inc/conn.php");
 $page = $_GET['page'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
