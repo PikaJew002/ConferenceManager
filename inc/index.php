1cc91fc264b4388e44c7f3a9b32509a27e1633d1
PikaJew002 <aaron@ironmthome.com>
2017-11-28 23:24:55 -0500

    Content addition to home page. Playing with div for professional look.

--- inc/index.php
+++ inc/index.php
@@ -1,4 +1,12 @@
-<p>
-  Well, here's a home page. <br />
-  (Some explanation of the function of the site)
-</p>
+<div id="container" style="">
+  <div id="header_box">
+    <div id="content">
+      <h1>The one-stop-shop for all of your conference needs</h1>
+      <p>
+        The Conference Manager provides a complete suite of solutions for your conference needs.
+        Paper reviews can register personalized accounts with a live bidding system for choosing
+        which papers they choose to review with restraints specified by you, the administrator.
+      </p>
+    </div>
+  </div>
+</div>
