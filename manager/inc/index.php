a5d4948324480d2a086f158a7c4f941437a957b9
PikaJew002 <aaron@ironmthome.com>
2017-11-30 01:08:44 -0500

    Added dynamic listing of conferences attached to admin.

--- manager/inc/index.php
+++ manager/inc/index.php
@@ -1,8 +1,15 @@
 <div id="container">
   <div id="conferences">
     <?php
-    
+    $query = "SELECT name FROM conferences WHERE admin=\"".$_SESSION['id']."\"";
+    $result = $mysqli->query($query);
+    if($result->num_rows >= 1) {
+      while($row = $result->fetch_assoc()) {
+        echo "<a href=\"\">".$row['name']."</a>\n";
+      }
+    }
     ?>
+    <a href="">Create Conference</a>
   </div>
   <div id="info">
   </div>
